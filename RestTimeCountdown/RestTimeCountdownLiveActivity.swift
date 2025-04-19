a4d23bc634b5642a4ed0e97755e3d283775d2b5a
Eliott Wantz <dev.eliottdev@gmail.com>
2025-04-19 10:21:54 -0400

    dynamic island live activity

--- RestTimeCountdown/RestTimeCountdownLiveActivity.swift
+++ RestTimeCountdown/RestTimeCountdownLiveActivity.swift
@@ -13,33 +13,83 @@ struct RestTimeCountdownLiveActivity: Widget {
   var body: some WidgetConfiguration {
     ActivityConfiguration(for: RestTimeCountdownAttributes.self) { context in
       // Lock screen/banner UI goes here
-      LockeScreenView(attributes: context.attributes, state: context.state)
+      let timerInterval =
+        context.attributes.startTime...Date().addingTimeInterval(TimeInterval(context.attributes.restTime))
+      LockeScreenView(attributes: context.attributes, state: context.state, timerInterval: timerInterval)
         .activityBackgroundTint(context.state.userAccentColor.opacity(0.8))
         .activitySystemActionForegroundColor(context.state.userAccentColor.contrastColor)
 
     } dynamicIsland: { context in
-      DynamicIsland {
+      let timerInterval =
+        context.attributes.startTime...Date().addingTimeInterval(TimeInterval(context.attributes.restTime))
+      return DynamicIsland {
         // Expanded UI goes here.  Compose the expanded UI through
         // various regions, like leading/trailing/center/bottom
         DynamicIslandExpandedRegion(.leading) {
-          Text("Leading")
+          VStack(alignment: .leading) {
+            HStack(spacing: 4) {
+              Text("Set \(context.attributes.nextSet)/\(context.attributes.totalSets)")
+                .font(.subheadline)
+              Text("•")
+                .font(.subheadline)
+              Text("\(context.attributes.nextReps) reps")
+                .font(.subheadline)
+              Text("•")
+                .font(.subheadline)
+              Text("\(context.attributes.nextWeight.formattedWeight(inLbs: context.state.displayWeightInLbs))")
+                .font(.subheadline)
+            }
+          }
+          .padding(.top)
         }
         DynamicIslandExpandedRegion(.trailing) {
-          Text("Trailing")
+          HStack {
+            Text(timerInterval: timerInterval)
+              .font(.system(size: 30, weight: .bold, design: .rounded))
+              .monospacedDigit()
+              .contentTransition(.numericText())
+              .foregroundColor(context.state.userAccentColor.foregroundColor)
+              .frame(maxWidth: 100)
+          }
+          .padding(.top)
         }
         DynamicIslandExpandedRegion(.bottom) {
-          Text("Bottom")
-          // more content
+          VStack {
+            HStack(alignment: .firstTextBaseline) {
+              Text("NEXT: ")
+              Text(context.attributes.nextExercise)
+                .font(.title)
+                .fontWeight(.bold)
+                .lineLimit(1)
+                .multilineTextAlignment(.center)
+            }
+
+            ProgressView(timerInterval: timerInterval) {
+            } currentValueLabel: {
+            }
+            .progressViewStyle(.linear)
+            .tint(context.state.userAccentColor.foregroundColor)
+            .scaleEffect(x: 1, y: 1.5, anchor: .center)
+          }
         }
       } compactLeading: {
-        Text("L")
+        Text("\(context.attributes.nextSet)/\(context.attributes.totalSets)")
+        .foregroundStyle(context.state.userAccentColor.foregroundColor)
       } compactTrailing: {
-        Text("T")
+        ProgressView(timerInterval: timerInterval) {
+        } currentValueLabel: {
+        }
+        .progressViewStyle(.circular)
+        .tint(context.state.userAccentColor.foregroundColor)
       } minimal: {
-        Text("M")
+        ProgressView(timerInterval: timerInterval) {
+        } currentValueLabel: {
+        }
+        .progressViewStyle(.circular)
+        .tint(context.state.userAccentColor.foregroundColor)
+        .scaleEffect(x: 1.5, y: 1.5, anchor: .center)
       }
-      //            .widgetURL(URL(string: "http://www.apple.com"))
-      .keylineTint(Color.red)
+      .keylineTint(context.state.userAccentColor)
     }
   }
 }
@@ -47,13 +97,12 @@ struct RestTimeCountdownLiveActivity: Widget {
 private struct LockeScreenView: View {
   let attributes: RestTimeCountdownAttributes
   let state: RestTimeCountdownAttributes.ContentState
+  let timerInterval: ClosedRange<Date>
 
   var body: some View {
-    let timerInterval = attributes.startTime...Date().addingTimeInterval(TimeInterval(attributes.restTime))
     VStack(alignment: .leading, spacing: 8) {
       VStack(alignment: .leading, spacing: 5) {
         HStack(alignment: .firstTextBaseline) {
-          Text("NEXT: ")
           Text(attributes.nextExercise)
             .font(.title)
             .fontWeight(.bold)
@@ -102,7 +151,7 @@ extension RestTimeCountdownAttributes {
   fileprivate static var preview: RestTimeCountdownAttributes {
     RestTimeCountdownAttributes(
       nextExercise: "Squat", nextSet: 2, totalSets: 3, nextReps: 10, nextWeight: 30.0, timerId: "abcde",
-      restTime: 120, startTime: .now)
+      restTime: 60, startTime: .now)
   }
 }
 
@@ -113,6 +162,24 @@ extension RestTimeCountdownAttributes.ContentState {
 }
 
 #Preview("Notification", as: .content, using: RestTimeCountdownAttributes.preview) {
+  RestTimeCountdownLiveActivity()
+} contentStates: {
+  RestTimeCountdownAttributes.ContentState.sample
+}
+
+#Preview("Compact", as: .dynamicIsland(.compact), using: RestTimeCountdownAttributes.preview) {
+  RestTimeCountdownLiveActivity()
+} contentStates: {
+  RestTimeCountdownAttributes.ContentState.sample
+}
+
+#Preview("Expanded", as: .dynamicIsland(.expanded), using: RestTimeCountdownAttributes.preview) {
+  RestTimeCountdownLiveActivity()
+} contentStates: {
+  RestTimeCountdownAttributes.ContentState.sample
+}
+
+#Preview("Minimal", as: .dynamicIsland(.minimal), using: RestTimeCountdownAttributes.preview) {
   RestTimeCountdownLiveActivity()
 } contentStates: {
   RestTimeCountdownAttributes.ContentState.sample
